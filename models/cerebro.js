141c2e2c8be46f05699acaebdfb305784f5a8a68
Brian Meraz <gigadrillbreaker@outlook.es>
2020-02-04 19:11:37 -0600

    04/02/2020 7:11 pm

--- models/cerebro.js
+++ models/cerebro.js
@@ -1,10 +1,30 @@
 var mongoose = require('mongoose');
 
 var modelSchema = mongoose.Schema({
-    description: String,
-    flavor: String,
-    price: String,
-    picture: String
+    description: {
+        type: String,
+        min: [10, 'La descripcion es muy corta'],
+        max: [50, 'La descripcion es muy larga'],
+        required: [true, 'Este campo es obligatorio!']
+    },
+    flavor: {
+        type: String,
+        min: [3, 'El tipo de sabor es muy corto'],
+        max: [20, 'El tipo de sabor es muy largo'],
+        required: [true, 'Este campo es obligatorio']
+    },
+    price: {
+        type: String,
+        min: [1, 'El precio es muy chico'],
+        max: [10, 'El precio es demasiado caro'],
+        required: [true, 'Este campo es obligatorio']
+    },
+    picture: {
+        type: String,
+        min: [0,'!!!!!'],
+        max: [100, 'Demasiados caracteres!'],
+        required: [true, 'Este campo es obligatorio']
+    }
 });
 
 var Cerebro = mongoose.model("Cerebro",modelSchema);
--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,16 @@ router.get('/', function(req, res, next) {
   
 });
 
-/* Get cerebros page. */
 router.get('/cerebros', function(req, res, next) {
   Cerebro.find().exec(function(error,cerebros){
     if(!error){
       res.render('cerebros/index', { viewCerebros: cerebros});
     }
-  })  
+  })
+  
 });
-/* Add new cerebros */
-router.get('/cerebros/add',function(req, res){
-  res.render('add_cerebros',{obj:''});
-});
-
-
-/* Add new zombie. */
 router.get('/zombies/add',function(req, res){
-  res.render('add',{msg:''});
+  res.render('add',{msg: ''});
 });
 
 router.post('/zombies/new',function(req, res){
@@ -43,9 +36,10 @@ router.post('/zombies/new',function(req, res){
   });
 
   nuevoZombie.save().then(function(){
-    res.render('add', {msg: '<div role="alert" class="alert alert-success">Se agrego el zombie con exito!</div>'});
+    res.render('add', {msg: 'Se agrego el zombie con exito!'});
     
-  });  
+  });
+  
 });
 
 
